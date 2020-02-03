e868a0df42344db7e87d50b26232d819759b2c49
Tetyana <tsergata@gmail.com>
2020-02-03 17:30:41 +0100

    ragionamento

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 $(document).ready(function() {
   var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0';
 
+  var arrayDate = [];
+  var date = moment("2018-01", "YYYYMM").daysInMonth();
+  console.log(date);
+  arrayDate.push(date);
+  console.log(arrayDate);
+
+
   // Chiamata Ajax
   $.ajax(
     {
