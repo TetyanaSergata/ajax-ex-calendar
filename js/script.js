ca11e6d77531c655f5863e690703b642da215ee7
Tetyana <tsergata@gmail.com>
2020-02-03 17:00:01 +0100

    chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // Creare un calendario dinamico con le festività.
+$(document).ready(function() {
+  var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0';
+
+  // Chiamata Ajax
+  $.ajax(
+    {
+      'url': url,
+      'method': 'Get',
+      'success': function(data, response) {
+      console.log(data);
+      console.log(response);
+    },
+    'error': function (request, state, errors) {
+      alert('errore' + errors);
+    }
+  });
+
+});
