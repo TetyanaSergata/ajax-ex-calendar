e74c981d16ebe54ae323396b1300e627fb10d4f8
Tetyana <tsergata@gmail.com>
2020-02-04 17:33:58 +0100

    lavoro sul template

--- js/script.js
+++ js/script.js
@@ -1,26 +1,47 @@
 // Creare un calendario dinamico con le festività.
 $(document).ready(function() {
-  var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0';
-
-  var arrayDate = [];
-  var date = moment("2018-01", "YYYYMM").daysInMonth();
-  console.log(date);
-  arrayDate.push(date);
-  console.log(arrayDate);
-
-
-  // Chiamata Ajax
-  $.ajax(
-    {
-      'url': url,
-      'method': 'Get',
-      'success': function(data, response) {
-      console.log(data);
-      console.log(response);
-    },
-    'error': function (request, state, errors) {
-      alert('errore' + errors);
-    }
-  });
-
+  // creo un ciclo per ig giorni del mese
+  // i = day;
+  for (var i = 1; i <= 31; i++) {
+    console.log(i);
+  }
+  // Template  Handlebars
+  var firstMonth = 'January';
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  var context = {
+    day: i,
+    month: firstMonth
+  };
+  var html = template(context);
+  // Spostiamo il contenuto dentro l'elemento con .append
+  $('.days').append(html);
 });
+
+
+//   var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0';
+//
+//
+//   var date = moment("2018-01", "YYYYMM").daysInMonth();
+//
+//
+//
+//
+//   // Chiamata Ajax
+//   $.ajax(
+//     {
+//       'url': url,
+//       'method': 'Get',
+//       'success': function(data, response) {
+//       // console.log(data);
+//       // console.log(response);
+//     },
+//     'error': function (request, state, errors) {
+//       alert('errore' + errors);
+//     }
+//   });
+//
+// });
+//
+//
+//  // Funzioni
